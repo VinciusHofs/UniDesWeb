8a2b66c9d858c363177272abd2702f8698e01c19
ViniHoffmann <124735617+VinciusHofs@users.noreply.github.com>
2024-08-26 22:09:59 -0300

    Add files via upload

---
+++ 26082024/paitam.py
@@ -0,0 +1,8 @@
+a = int(input("informe um número: "))
+b = int(input("informe outro número:"))
+if a > b:
+    dif = a-b
+    print("A diferença de ",a, "para ",b, "é de ",dif)
+else:
+    dif = b-a
+    print("A diferença de ",b, "para ",a, "é de ",dif)
